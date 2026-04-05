6abc34813622cba1e4739fc0dc221f7336378ff9
Suraj Agrahari <surajagrahari265@gmail.com>
2026-04-05 19:40:25 +0530

    Fix nested git repository in frontend and track files properly

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+
+import Hero from "../components/section/Hero";
+import { WorldMapDemo } from "../components/section/Worldmap";
+
+export default function Home() {
+    return (
+        <div>
+            <Hero />
+            <WorldMapDemo />
+        </div>
+    );
+}
